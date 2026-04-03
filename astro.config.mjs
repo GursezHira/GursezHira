d7f471e77e308495e6aa9d8b895769039e860399
Harman Singh Hira <cartw3@gmail.com>
2026-04-03 17:00:19 +1300

    Building

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,13 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
+// export default defineConfig({
+//   site: 'https://gursez.hira.im',
+//   integrations: [sitemap()],
+// });
+
 export default defineConfig({
-  site: 'https://gursez.hira.im',
+  site: 'https://gursezhira.github.io',
+  base: '/GursezHira/',
   integrations: [sitemap()],
 });
